99e3e36fba370dbaa12268ccfdd3db29a5915752
CoderVu <vunguyen.170803@gmail.com>
2024-07-23 16:47:12 +0700

    update edit hotell

--- src/components/utils/axiosConfig.js
+++ src/components/utils/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8081',
+  baseURL: 'https://be-hotel.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
